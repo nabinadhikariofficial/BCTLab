9999c14664ae2fb8e83aa87f2590f4ef7fc0c857
nabin adhikari <34744818+nabinadhikari70@users.noreply.github.com>
2019-05-28 16:42:32 +0545

    Add files via upload

---
+++ Data_Structure_And_Algorithm/LinearQueueTopAndTailVarying.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<conio.h>
+#define maxsize 3
+void enq();
+void deq();
+void dis();
+int front,rear,queue[maxsize];
+
+void main() {
+    int ch;
+    front=0;
+    rear=-1;
+    while(1)
+    {
+        printf("\n\nQueue:\n\n\t1. ENQUEUE\n\n\t2. DEQUEUE\n\n\t3. DISPLAY\n\n\t4.EXIT");
+        printf("\n-------------------------------");
+        printf("\n\nEnter your choice :- ");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1:
+                enq();
+                break;
+            case 2:
+                deq();
+                break;
+            case 3:
+                dis();
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("\n\n\tEnter correct choice..........");
+        }
+    }
+}
+
+void enq() {
+    int data;
+    if(rear>=maxsize-1)
+        printf("\n\tQueue is FULL");
+    else
+    {
+        printf("\n\tEnter data :- ");
+        scanf("%d",&data);
+        rear++;
+        queue[rear]=data;
+        printf("\n\tDATA ENQUEUED");
+    }
+
+}
+void deq() {
+    int data;
+    if(front>rear)
+        printf("\n\tQueue is EMPTY");
+    else
+    {
+        data=queue[front];
+        front++;
+        printf("\n\tData = %d",data);
+        printf("\n\n\tDATA DEQUEUED");
+    }
+}
+
+void dis() {
+    int i;
+    if(front>rear)
+        printf("\n\tQueue is EMPTY");
+    else
+    {
+        printf("\n(FRONT)\t\t");
+        for(i=front;i<=rear;i++) {
+                printf("%d\t",queue[i]);
+        }
+        printf("(REAR)");
+        if(rear>=maxsize-1)
+            printf("\n\n\tQueue FULL");
+
+    }
+
+}
