f3b82021ce532d9a30cd2a53e2fe051de8096948
Nabin Adhikari <34744818+nabinadhikari70@users.noreply.github.com>
2019-06-02 17:01:45 +0545

    Add files via upload

---
+++ Numerical_Method/EigenPowerMethod.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+float a[20][20],x[40],sum=0,error,lambda_old,lambda_new;
+int i,j,n;
+
+void pri()
+{
+
+    printf("\n");
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("%.2f\t",a[i][j]);
+        }
+            printf("\t|%.2f\t|=\t|%.2f|",x[i],x[n+i]);
+            printf("\n");
+    }
+    printf("\nlambda is %f\n",lambda_new);
+}
+
+void main()
+{
+
+    int i,j;
+    printf("Enter Order of matrix:");
+    scanf("%d",&n);
+    printf("Enter tolerance error:");
+    scanf("%f",&error);
+    printf("\nEnter Initial guess:\n");
+    for(i=1;i<=n;i++)
+    {
+        printf("X[%d]=",i);
+        scanf("%f",&x[i]);
+    }
+    printf("\nEnter Matrix element row wise:\n");
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("A[%d][%d]=",i,j);
+            scanf("%f",&a[i][j]);
+        }
+    }
+    lambda_old=1.0;
+    while(1)
+    {
+        for(i=1;i<=n;i++)
+        {
+            sum=0.0;
+            for(j=1;j<=n;j++)
+            {
+                sum=sum+a[i][j]*x[j];
+            }
+            x[n+i]=sum;
+        }
+        lambda_new=x[n+1];
+        for(i=1;i<=n;i++)
+        {
+            if(lambda_new<x[n+i])
+                lambda_new=x[n+i];
+        }
+        pri();
+        for(i=1;i<=n;i++)
+        {
+            x[i]=x[n+i]/lambda_new;
+        }
+        if(fabs(lambda_old-lambda_new)>error)
+            lambda_old=lambda_new;
+        else
+            break;
+    }
+    getch();
+}
